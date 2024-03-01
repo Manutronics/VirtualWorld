8a5a5531662e17d6bf96e6fc05bff42ffc07b8aa
Manutronics <manutronics@gmail.com>
2024-03-01 15:35:25 -0300

    Lesson 6 finished

--- js/world.js
+++ js/world.js
@@ -41,7 +41,7 @@ class World{
     {
         const points = [
             ...this.roadBorders.map((s) => [s.p1, s.p2]).flat(),
-            ...this.buildings.map((b) => b.points).flat()
+            ...this.buildings.map((b) => b.base.points).flat()
         ];
         const left = Math.min(...points.map((p) => p.x));
         const right = Math.max(...points.map((p) => p.x));
@@ -49,7 +49,7 @@ class World{
         const bottom = Math.max(...points.map((p) => p.y));
 
         const illegalPolys = [
-            ...this.buildings,
+            ...this.buildings.map((b) => b.base),
             ...this.envelopes.map((e) => e.poly)
         ];
 
@@ -78,7 +78,7 @@ class World{
             {
                 for(const tree of trees)
                 {
-                    if(distance(tree, p) < this.treeSize)
+                    if(distance(tree.center, p) < this.treeSize)
                     {
                         keep = false;
                         break;
@@ -103,7 +103,7 @@ class World{
 
             if(keep)
             {
-                trees.push(p);
+                trees.push(new Tree(p, this.treeSize));
                 tryCount = 0;
             }
             tryCount++;
@@ -179,10 +179,10 @@ class World{
             }
         }
 
-        return bases;
+        return bases.map((b) => new Building(b));
     }
 
-    draw(ctx)
+    draw(ctx, viewPoint)
     {
         for(const env of this.envelopes)
         {
@@ -196,14 +196,24 @@ class World{
         {
             seg.draw(ctx, { color: 'white', width: 4 })
         }
-        for(const tree of this.trees)
+
+        const items = [...this.buildings, ...this.trees];
+        items.sort(
+            (a, b) => b.base.distanceToPoint(viewPoint) - a.base.distanceToPoint(viewPoint)
+        );
+        for(const item of items)
         {
-            tree.draw(ctx, { size: this.treeSize, color: "rgba(0,0,0,0.5)" });
+            item.draw(ctx, viewPoint);
         }
-        for(const bld of this.buildings)
-        {
-            bld.draw(ctx);
-        }
+        // for(const tree of this.trees)
+        // {
+        //     tree.draw(ctx, viewPoint);
+        // }
+        // for(const bld of this.buildings)
+        // {
+        //     bld.draw(ctx, viewPoint);
+        // }
+
         // for(const int of this.intersections)
         // {
         //     int.draw(ctx, {color: 'red', size: 6 });
